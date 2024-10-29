6c6f79939d91be8bf07cd722038140322e68d67a
notaisha <aishaajanahi@gmail.com>
2024-10-29 14:59:30 -0400

    init

--- Create_schedule.py
+++ Create_schedule.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from streamlit_calendar import calendar
+#from streamlit_calendar import calendar
 from datetime import datetime, timedelta
 from collections import Counter
 import matplotlib.pyplot as plt
